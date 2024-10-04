47776885cac713e1ca3c7284b3f4f0a3847c1944
Carduus0 <s.carduus@gmail.com>
2024-10-04 21:19:03 +0300

    init: start nodejs-basics-task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,8 @@
+import fs from 'node:fs'
+import path from 'path'
 const create = async () => {
     // Write your code here 
+    
 };
 
 await create();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import {promises as fs} from 'fs'
+import path from 'path'
 const read = async () => {
     // Write your code here 
+    const fileName = 'fileToRead.txt'
+    const foldername = 'files'
+    const filePath = path.join(foldername, fileName)
+    try{
+        await fs.access(filePath)
+        const outcome = await fs.readFile(filePath, 'utf8')
+        console.log(outcome)
+    }catch(err){
+      console.error('FS operation failed')
+    }
+
 };
 
 await read();
