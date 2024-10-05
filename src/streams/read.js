bbbc734848c264d5847985455f44c2f5ebff4166
Carduus0 <s.carduus@gmail.com>
2024-10-05 23:54:45 +0300

    feat: read, write, transform implemented

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import fs from 'fs'
+import path from 'path'
+import {fileURLToPath } from 'url'
+
+const __fileName = fileURLToPath(import.meta.url)
+const __dirName = path.dirname(__fileName)
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirName, 'files', '/fileToRead.txt' )
+    // Write your code here
+    const readableStream = fs.createReadStream(filePath, {encoding: 'utf8'}) 
+    readableStream.pipe(process.stdout)
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import fs from 'fs'
+import path from 'path'
+import {fileURLToPath } from 'url'
+
+const __fileName = fileURLToPath(import.meta.url)
+const __dirName = path.dirname(__fileName)
+
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = path.join(__dirName, 'files', 'fileToWrite.txt' ) 
+    const stream = fs.createWriteStream(filePath)
+     
+    process.stdin.pipe(stream)
 };
 
 await write();
