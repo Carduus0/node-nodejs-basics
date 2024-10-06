0877140c23896332b856eb5df97bf4add8554c5f
Carduus0 <s.carduus@gmail.com>
2024-10-06 12:03:56 +0300

    feat: cli env.js and args.js implemented

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+const argS = process.argv.slice(2);
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const props = {};
+  for (let i = 0; i < argS.length; i += 2) {
+    const propName = argS[i].replace(/^--/, "");
+    const propVal = argS[i + 1];
+    props[propName] = propVal;
+  }
+  Object.keys(props).forEach((propName) => {
+    console.log(`${propName} is ${props[propName]}`);
+  });
 };
 
 parseArgs();
