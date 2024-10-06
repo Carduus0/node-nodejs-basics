b910458133216d618ba0bf73a0d5b42c1cbbc1ee
Carduus0 <s.carduus@gmail.com>
2024-10-06 17:48:20 +0300

    feat: cp.js implemented

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import {spawn} from 'child_process'
+const arg1 = 121212
+const arg2 = 343434
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const child = spawn('node', ['src/cp/files/script.js', ...args], {stdio: ['pipe', 'pipe', 'pipe', 'ipc']})
+    process.stdin.pipe(child.stdin)
+    child.stdout.pipe(process.stdout)
+
+    child.on('exit', (code)=>{
+        console.log(code);
+        
+    })
+    child.on('error', (err)=>{
+        console.log(err);
+        
+    })
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([arg1, arg2] /* [someArgument1, someArgument2, ...] */);
+
+// CLOSE for a child process to be terminated correctly
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,9 @@
+import {Worker} from 'worker_threads'
+import os from 'os'
+import path from 'path'
 const performCalculations = async () => {
     // Write your code here
+
 };
 
 await performCalculations();
